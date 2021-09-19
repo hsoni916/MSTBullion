8acd6b4ebc7c07c28ee35ed320a2b7ad7c0c2f0f
Harsh <pataliaharsh948@gmail.com>
2021-09-19 16:50:45 +0530

    Commit 2

--- app/src/main/java/com/example/mstbullion/MainActivity.java
+++ app/src/main/java/com/example/mstbullion/MainActivity.java
@@ -18,7 +18,6 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -27,18 +26,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import io.socket.client.IO;
+import io.socket.client.Manager;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
+import io.socket.engineio.client.Transport;
 
 public class MainActivity extends Fragment {
-    ImageView GDI,SDI;
+    ImageView GDI,SDI,GDIMCX,SDIMCX;
     Animation increase,decrease;
     RecyclerView MetalList;
     MetalListAdapter metalListAdapter;
     List<String> bullionlist;
     private Context context;
     TextView GoldUsdTV,SilverUsdTV,InrUsdTV,GoldMcxTV,SilverMcxTV;
-
+    int goldspot,silverspot;
     MainActivity(){
 
     }
@@ -63,6 +64,21 @@ public class MainActivity extends Fragment {
         View view = inflater.inflate(R.layout.activity_main, container, false);
         context = getContext();
         mSocket.connect();
+        mSocket.io().on(Manager.EVENT_TRANSPORT, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                Transport transport = (Transport) args[0];
+                transport.on(Transport.EVENT_ERROR, new Emitter.Listener() {
+                    @Override
+                    public void call(Object... args) {
+                        Exception e = (Exception) args[0];
+                        Log.e("TAG", "Transport error " + e);
+                        e.printStackTrace();
+                        e.getCause().printStackTrace();
+                    }
+                });
+            }
+        });
         mSocket.on("XAUUSD", GoldUsd);
         mSocket.on("XAUUSDI", GoldUsdIncrease);
         mSocket.on("XAUUSDD", GoldUsdDecrease);
@@ -73,8 +89,11 @@ public class MainActivity extends Fragment {
         mSocket.on("INRSPOTI", InrUsd);
         mSocket.on("INRSPOTD", InrUsd);
         mSocket.on("spotup", spotupdate);
+        mSocket.on("spotupi", spotupdatei);
         GDI = view.findViewById(R.id.GDI);
         SDI = view.findViewById(R.id.SDI);
+        GDIMCX = view.findViewById(R.id.GDIMCX);
+        SDIMCX = view.findViewById(R.id.SDIMCX);
         GoldUsdTV = view.findViewById(R.id.GoldSpotPrices);
         SilverUsdTV = view.findViewById(R.id.SilverSpotPrices);
         InrUsdTV = view.findViewById(R.id.INRSpotPrices);
@@ -101,15 +120,17 @@ public class MainActivity extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         mSocket.disconnect();
-        mSocket.off("XAUUSD",GoldUsd);
-        mSocket.off("XAUUSDI",GoldUsdIncrease);
-        mSocket.off("XAUUSDD",GoldUsdDecrease);
-        mSocket.off("XAGUSD",SilverUsd);
-        mSocket.off("XAGUSDI",SilverUsdIncrease);
-        mSocket.off("XAGUSDD",SilverUsdDecrease);
-        mSocket.off("INRSPOT",InrUsd);
-        mSocket.off("INRSPOTI",InrUsd);
-        mSocket.off("INRSPOTD" ,InrUsd);
+        mSocket.off("XAUUSD", GoldUsd);
+        mSocket.off("XAUUSDI", GoldUsdIncrease);
+        mSocket.off("XAUUSDD", GoldUsdDecrease);
+        mSocket.off("XAGUSD", SilverUsd);
+        mSocket.off("XAGUSDI", SilverUsd);
+        mSocket.off("XAGUSDD", SilverUsd);
+        mSocket.off("INRSPOT", InrUsd);
+        mSocket.off("INRSPOTI", InrUsd);
+        mSocket.off("INRSPOTD", InrUsd);
+        mSocket.off("spotup", spotupdate);
+        mSocket.off("spotupi", spotupdatei);
     }
 
     private final Emitter.Listener GoldUsd = new Emitter.Listener() {
@@ -118,6 +139,7 @@ public class MainActivity extends Fragment {
             getActivity().runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
+                    Log.d("XAU,USD","True");
                     GoldUsdTV.setText(args[0].toString());
                 }
             });
@@ -211,7 +233,7 @@ public class MainActivity extends Fragment {
                 @Override
                 public void run() {
                     String data = args[0].toString();
-                    data = data.substring(2);
+                    Log.d("Data:",data);
                     JSONObject jsonObject = new JSONObject();
                     try {
                         jsonObject= new JSONObject(data);
@@ -221,10 +243,53 @@ public class MainActivity extends Fragment {
                     try {
                         Log.d("Spot:", jsonObject.get("InsSymbol").toString());
                         if(jsonObject.get("InsSymbol").toString().contains("GOLD")){
-                            GoldMcxTV.setText(jsonObject.get("Sell").toString());
+                            if(goldspot==0) {
+                                String sell = jsonObject.get("Sell").toString().replace(",","");
+                                goldspot = Integer.parseInt(sell);
+                                GoldMcxTV.setText(jsonObject.get("Sell").toString());
+                            }else{
+                                int currentprice = Integer.parseInt(jsonObject.get("Sell").toString().replace(",",""));
+                                if(goldspot==currentprice){
+                                    GoldMcxTV.setText(String.valueOf(currentprice));
+                                }
+                                if(goldspot>currentprice){
+                                    goldspot = currentprice;
+                                    GoldMcxTV.setText(String.valueOf(currentprice));
+                                    GDIMCX.setImageResource(R.drawable.down);
+                                    GDIMCX.startAnimation(decrease);
+                                }else{
+                                    goldspot = currentprice;
+                                    GoldMcxTV.setText(String.valueOf(currentprice));
+                                    GDIMCX.setImageResource(R.drawable.up);
+                                    GDIMCX.startAnimation(increase);
+                                }
+                            }
                         }
                         if(jsonObject.get("InsSymbol").toString().contains("SILVER")){
-                            SilverMcxTV.setText(jsonObject.get("Sell").toString());
+                            if(silverspot==0){
+                                String sell = jsonObject.get("Sell").toString().replace(",","");
+                                silverspot = Integer.parseInt(sell);
+                                SilverMcxTV.setText(sell);
+                            }else{
+                                int currentprice = Integer.parseInt(jsonObject.get("Sell").toString().replace(",",""));
+                                if(silverspot==currentprice){
+                                    SilverMcxTV.setText(String.valueOf(currentprice));
+                                }
+                                if(silverspot>currentprice){
+                                    silverspot = currentprice;
+                                    SilverMcxTV.setText(String.valueOf(currentprice));
+                                    SDIMCX.setImageResource(R.drawable.down);
+                                    SDIMCX.startAnimation(decrease);
+                                    //fire increase animation
+                                }else{
+                                    silverspot = currentprice;
+                                    SilverMcxTV.setText(String.valueOf(currentprice));
+                                    SDIMCX.setImageResource(R.drawable.up);
+                                    SDIMCX.startAnimation(increase);
+
+                                    //fire decrease animation
+                                }
+                            }
                         }
                         Log.d("Spot:", jsonObject.get("Buy").toString());
                     } catch (JSONException e) {
@@ -235,4 +300,30 @@ public class MainActivity extends Fragment {
         }
     };
 
+    private final Emitter.Listener spotupdatei = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d("spotupi","True");
+                    JSONObject jsonObject = new JSONObject();
+                    try {
+                        jsonObject = new JSONObject(args[0].toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (!jsonObject.get("rows").toString().isEmpty()) {
+                            jsonObject = jsonObject.getJSONObject("rows");
+                            GoldMcxTV.setText(jsonObject.getJSONObject("GOLD").get("last_price").toString());
+                            SilverMcxTV.setText(jsonObject.getJSONObject("SILVER").get("last_price").toString());
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    };
 }
